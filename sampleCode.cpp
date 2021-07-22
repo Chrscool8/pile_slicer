1e1ba0617848273392de027b0dbe362590fcb5c6
Chris Bradel <ChrisBradel14@gmail.com>
2021-07-22 07:32:57 -0400

    extendable

--- sampleCode.cpp
+++ sampleCode.cpp
@@ -3,7 +3,7 @@
 #include <opencv2/opencv.hpp>
 #include <filesystem>
 
-using namespace std;
+//using namespace std;
 using namespace cv;
 namespace fs = std::filesystem;
 
@@ -11,34 +11,34 @@ int main(int argc, char** argv)
 {
 	if (argc >= 3)
 	{
-		string file = argv[1];
+		std::string file = argv[1];
 		Mat image = imread(file, IMREAD_UNCHANGED);
 		int depth = atoi(argv[2]);
 
 		file = file.substr(0, file.find_last_of("."));
 
 		bool extend = false;
-
-		vector<Mat> channels(3);
-		split(image, channels);
-		cout << "> " << to_string(channels[0].at<uchar>(16, 16)) << endl;
-		cout << "> " << to_string(channels[1].at<uchar>(16, 16)) << endl;
-		cout << "> " << to_string(channels[2].at<uchar>(16, 16)) << endl;
-		cout << "> " << to_string(channels[3].at<uchar>(16, 16)) << endl;
-
 		if (argc == 4)
 		{
-			extend = (argv[3] == "true");
+			if (std::string(argv[3]) == std::string("true"))
+			{
+				std::cout << "extend" << std::endl;
+				extend = true;
+			}
+			else
+				std::cout << "don't extend" << std::endl;
 		}
+		else
+			std::cout << "don't extend" << std::endl;
 
 		if (!image.empty())
 		{
-			cout << image.type() << endl;
+			//std::cout << image.type() << std::endl;
 
 			//if (image.channels() == 3)
 
 			fs::create_directories("output");
-			vector<Mat> images;
+			std::vector<Mat> images;
 
 			for (int i = 0; i < image.size().height; i++)
 			{
@@ -48,17 +48,32 @@ int main(int argc, char** argv)
 				for (int j = 0; j < depth; j++)
 				{
 					Mat row = image.row(i);
-					if (j == 0)
+					if (j == 0 || extend)
 					{
 						row.copyTo(current.row(image.size().height / 2 + depth / 2 - j));
 					}
 					else
 					{
-						for (int k = 0; k < row.size().height; k++)
+						for (int k = 0; k < row.size().width; k++)
 						{
-							cout << k << endl;
+							//cout << k << endl;
 
-							row.at<Vec4i>(0, k);
+							std::vector<Mat> channels(3);
+							split(row, channels);
+							//cout << "> " << to_string(channels[0].at<uchar>(0, k)) << endl;
+							//cout << "> " << to_string(channels[1].at<uchar>(0, k)) << endl;
+							//cout << "> " << to_string(channels[2].at<uchar>(0, k)) << endl;
+							//cout << "> " << to_string(channels[3].at<uchar>(0, k)) << endl;
+
+							if (channels[3].at<uchar>(0, k) != 0)
+							{
+								channels[0].at<uchar>(0, k) = 0;
+								channels[1].at<uchar>(0, k) = 0;
+								channels[2].at<uchar>(0, k) = 0;
+								merge(channels, row);
+							}
+
+							//row.at<Vec4u>(0, k);
 							//Vec4d point = row.at<Vec4d>(Point(0,k));
 							//if (row.at<Vec4>(k, 0).val[3] != 0)
 							//	row.at<Vec4f>(k, 0) = Vec4f(0, 0, 0, 1);
@@ -69,18 +84,18 @@ int main(int argc, char** argv)
 
 				images.push_back(current);
 
-				string output = "output/" + string(file) + "_3d_" + to_string(i) + ".png";
-				cout << output << endl;
+				std::string output = "output/" + std::string(file) + "_3d_" + std::to_string(image.size().height - i - 1) + ".png";
+				std::cout << output << std::endl;
 				imwrite(output, current);
 			}
 		}
 		else
 		{
-			cout << "Couldn't load pic";
+			std::cout << "Couldn't load pic" << std::endl;
 		}
 	}
 	else
-		cout << "./pile_slicer.exe <filename> <depth>" << endl;
+		std::cout << "./pile_slicer.exe <filename> <depth> (<depth color>)" << std::endl;
 
 	return 0;
 }
